64faa64dfafbaee51714cd00057e5ee9284cdf32
Deothan <mads@deothan.dk>
2015-04-05 19:08:06 +0200

    Traverse and Search

--- src/DictBinTree.java
+++ src/DictBinTree.java
@@ -12,7 +12,7 @@ public class DictBinTree implements Dict{
         
         while(x != null){
             y = x;
-            if(node.getKey() < x.getKey()){
+            if(node.getKey() <= x.getKey()){
                 x = x.getLeft();
             }
             else{
@@ -28,16 +28,46 @@ public class DictBinTree implements Dict{
         else{
             y.setRight(node);
         }
+        size++;
     }
 
     @Override
     public int[] orderedTraversal() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Node x = root;
+        int[] numbers = new int[size];
+        int i = 0;
+        
+        inorderTreeWalk(x, numbers, i);
+        
+        return numbers;
+    }
+    
+    private void inorderTreeWalk(Node x, int[] numbers, int i){
+        while(x != null){
+            inorderTreeWalk(x.getLeft(), numbers, i);
+            numbers[i] = x.getKey();
+            i++;
+            inorderTreeWalk(x.getRight(), numbers, i);
+        } 
     }
 
     @Override
     public boolean search(int k) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Node y = null, x = root;
+        
+        while(x != null){
+            y = x;
+            if(k < x.getKey()){
+                x = x.getLeft();
+            }
+            else if(k == x.getKey()){
+                return true;
+            }
+            else{
+                x = x.getRight();
+            }
+        }
+        return false;        
     }
     
 }
