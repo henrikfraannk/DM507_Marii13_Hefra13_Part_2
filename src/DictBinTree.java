11b54d9d0a77a238827e0c2c9354851d51c29eb3
Henrik Frank <frank@disko-fd.dk>
2015-04-05 15:17:52 +0200

    Init
    pik

---
+++ src/DictBinTree.java
@@ -0,0 +1,25 @@
+public class DictBinTree implements Dict{
+    
+    public DictBinTree DictBinTree(){
+        
+        DictBinTree dbt = new DictBinTree();
+        
+        return dbt;
+    }
+
+    @Override
+    public void insert(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int[] orderedTraversal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean search(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
